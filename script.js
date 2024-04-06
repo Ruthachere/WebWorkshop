23b8bfd1eda10370f935b71075a56b82ab58acdd
Ruthachere <akoruth22@gmail.com>
2024-04-06 20:01:09 +0100

    continuation of website

---
+++ script.js
@@ -0,0 +1,11 @@
+// const access = document.querySelector('.access');
+// const body = document.querySelector('.grids');
+// access.addEventListener('click', () => {
+//   if (access && body) {
+//     body.style.backgroundImage = `url("https://feelgrounded.com/cdn/shop/files/IMG_4386-2215.jpg?v=1706051403")`;
+// } else {
+//     // Handle the case where elements are not found (optional)
+//     console.error("Elements with classes '.access' and '.grid-item3' not found.");
+// }
+
+// });
